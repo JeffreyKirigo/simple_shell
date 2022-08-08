31cde75b147f3813bcff722ecff243366a63d0b7
JeffreyKirigo <jeffreyem99@gmail.com>
2022-08-08 14:36:25 +0000

    Added String functions

--- shell.h
+++ shell.h
@@ -10,4 +10,17 @@
 #include <errno.h>
 #include <signal.h>
 
+/* String Functions */
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+/* prototypes for the program */
+int main(void);
+
+
 #endif
